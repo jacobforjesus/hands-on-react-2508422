114128b34827e4ed22877d32b5b4f683bac83e2b
jacobforjesus <156459667+jacobforjesus@users.noreply.github.com>
2024-01-18 00:42:59 +0000

    finished 1_04

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -1,7 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+const pageTitle = document.title;
 
 export default () => {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    if (count > 0) {
+      count && (document.title = `${pageTitle}--${count}`)  // parenthesis only get executed if `count` is truthy
+    }
+  })
+
   return (
     <button className="outline" onClick={() => setCount(count + 1)}>
       {count === 0 ? "Click to support": `Supported ${count} times`}
